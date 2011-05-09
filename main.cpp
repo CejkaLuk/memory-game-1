d61ed61d1ecef0c63e693199404f90829725541d
Timur Kristóf <venemo@msn.com>
2011-05-09 12:46:03 +0200

    Setting up random seed

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <QtGui/QApplication>
+#include <time.h>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
@@ -6,6 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    qsrand(time(0));
 
     return a.exec();
 }
